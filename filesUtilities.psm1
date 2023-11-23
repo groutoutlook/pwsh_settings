48e1667d718b26515f22ba85a5134a3fcc3e053f
groutoutlook <groutlloyd@outlook.com>
2023-11-23 11:36:44 +0700

    :pencil2: Fix typo for commit.
    Add cdClip function in filesUtilities and rebase default at32 and mm32 dir.

--- filesUtilities.psm1
+++ filesUtilities.psm1
@@ -2,8 +2,8 @@
 
 
 
-$global:at_dir = "C:\ProgramDataD\Artery\Repos\AT32F435RGT7 Demo\project\at_start_f435\examples_v37\TEST\TEST\src\"
-$global:mm_dir = "C:\ProgramDataD\MindMotion\Repos\DshanMCU-PitayaLite-master\2_temp_proj\pitaya-c\12_uart_irq\Project\"
+$global:at_dir = "C:\ProgramDataD\Artery\Proj\TEST\src\"
+$global:mm_dir = "C:\ProgramDataD\MindMotion\Proj\12_uart_irq\Project\"
 
 function copyFilestoKeil($Destination, $Source = "C:\ProgramDataD\Visual Studio\ConsoleApplication1\ConsoleApplication1\")
 {
