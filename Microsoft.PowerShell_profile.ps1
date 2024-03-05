ce241189c202c2aeab9a2064b8e2a8c3531de47e
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-03-05 12:58:48 +0700

    :sparkles: Add Show-Window function
    and obs alias for quick find obsidian

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -379,6 +379,45 @@ function initProfileEnv
 	$Env:PSModulePath += ";" + $Env:p7settingDir 
 }
 
+function Show-Window
+{
+	param(
+		[Parameter(Mandatory)]
+		[string] $ProcessName
+	)
+	[alias("shw")]
+
+	# As a courtesy, strip '.exe' from the name, if present.
+	$ProcessName = $ProcessName -replace '\.exe$'
+
+	# Get the ID of the first instance of a process with the given name
+	# that has a non-empty window title.
+	# NOTE: If multiple instances have visible windows, it is undefined
+	#       which one is returned.
+	$procId = (Get-Process -ErrorAction Ignore $ProcessName).Where({ $_.MainWindowTitle }, 'First').Id
+
+	if (-not $procId)
+	{ Throw "No $ProcessName process with a non-empty window title found." 
+	}
+
+	# Note: 
+	#  * This can still fail, because the window could have been closed since
+	#    the title was obtained.
+	#  * If the target window is currently minimized, it gets the *focus*, but is
+	#    *not restored*.
+	#  * The return value is $true only if the window still existed and was *not
+	#    minimized*; this means that returning $false can mean EITHER that the
+	#    window doesn't exist OR that it just happened to be minimized.
+	$null = (New-Object -ComObject WScript.Shell).AppActivate($procId)
+}
+
+function obs
+{
+	Show-Window("Obsidian.exe")	
+}
+
+
+
 function initChat
 { #for environment variable.
 	$Env:ZaloDir = "C:\Users\ADMIN\AppData\Local\Programs\Zalo/"
@@ -393,6 +432,11 @@ function zl()
 	chrome https://chat.zalo.me
 } 
 
+function hn()
+{
+	Start-Process https://news.ycombinator.com/
+
+}
 
 . C:\Users\COHOTECH\AppData\Roaming\dystroy\broot\config\launcher\powershell\br.ps1
 #p7in
