df7b738d10693f812aa078b71e5b7c30d7d4baa8
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-03-12 21:12:07 +0700

    :sparkles: Add :nm to open a new pwsh instance
    with module preload

--- quickVimAction.psm1
+++ quickVimAction.psm1
@@ -10,8 +10,19 @@ function :q
 
 # Quick way to reload profile and turn back to the default pwsh
 # There's some other effects, so I may need to dig further I think?
-function :new
+function :n($p7 = 0) 
 {
-  pwsh && :q
+  if($p7 -eq 0)
+  {
+    pwsh && :q
+  } else
+  {
+    pwsh -Noexit -Command "p7 && p7mod" && :q
+  }
+}
+
+function :nm
+{
+  :n 7
 }
 
