8b0b138d4469b73194f07cbf5eb10d14788ed5e6
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-03-19 02:09:38 +0700

    :technologist: ft: make git clone clipboard become gccb

--- quickGitAction.psm1
+++ quickGitAction.psm1
@@ -3,24 +3,48 @@
 #Add-PoshGitToProfile -AllHosts
 
 
-function quickInitGit{
+function quickInitGit
+{
 	git init && git add * && gitmoji -c
 }
 
-function openWebRemote{
+function openWebRemote
+{
 	chrome (git remote get-url origin)
 }
 
+function gitCloneClipboard($link = (Get-Clipboard))
+{
+	if ($link -match "^https")
+ {
+
+		git clone (Get-Clipboard)
+	} else
+	{
+		Write-Host "Not a link." -ForegroundColor Red
+	}
+
+}
+Set-Alias -Name gccb -Value gitCloneClipboard
 
 
-
-function copyFilesFromOnlineRepos($URI = "", $gitDoc = "" , $OutFile =""){
-	if($URI -eq ""){$processedURI = [URI](Get-Clipboard)}
-	else{$processedURI = [URI]$URI }
-	if($gitDoc -eq ""){$finalName = $processedURI.Segments[-1]}
-	else{$finalName = $gitDoc}
-	if($OutFile -eq ""){$destinationFile = $finalName}
-	else{$destinationFile = $OutFile}
+function copyFilesFromOnlineRepos($URI = "", $gitDoc = "" , $OutFile ="")
+{
+	if($URI -eq "")
+	{$processedURI = [URI](Get-Clipboard)
+ } else
+	{$processedURI = [URI]$URI 
+ }
+	if($gitDoc -eq "")
+	{$finalName = $processedURI.Segments[-1]
+ } else
+	{$finalName = $gitDoc
+ }
+	if($OutFile -eq "")
+	{$destinationFile = $finalName
+ } else
+	{$destinationFile = $OutFile
+ }
 	iwr -uri $processedURI -OutFile ./$destinationFile && bat ./$destinationFile
 }
 Set-Alias -Name cpGit -Value copyFilesFromOnlineRepos -Scope Global
