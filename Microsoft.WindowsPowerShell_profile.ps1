93c112ac4bc018405d89f8398443fdd25dd2f652
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2025-04-10 12:16:10 +0700

    feat: add default WindowsPowerShell_profile for developer profile.

---
+++ Microsoft.WindowsPowerShell_profile.ps1
@@ -0,0 +1,11 @@
+Set-Alias pwsh "C:\Program Files\PowerShell\7-preview\pwsh.exe"
+Set-Alias -Name r -Value just -Scope Global -Option AllScope
+Set-Alias -Name :q -Value exit -Scope Global -Option AllScope
+function rr{
+    r r
+}
+
+function rb{
+    r build
+}
+
